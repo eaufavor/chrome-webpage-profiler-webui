bb4d8d33e43f896d34655145673e92946b0caf32
Yuchen Wu <ywu@akamai.com>
2015-07-09 13:36:37 -0400

    Add anaylze code

--- agent/agent.py
+++ agent/agent.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 #http://www.acmesystems.it/python_httpd
 from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
-import json, subprocess, os, time, urlparse
+import json, subprocess, os, time, urlparse, re
 
 HELLO_MESSAGE = {'message':'hello, please use JSON via POST!'}
 ERROR_JSON_MESSAGE = {'message':'POST content type must be application/json!'}
@@ -9,10 +9,18 @@ ERROR_BADJSON_MESSAGE = {'message':'POST content must be valid json!'}
 ERROR_BADPASS_MESSAGE = {'message':'Wrong secret key!'}
 ERROR_CMDERROR_MESSAGE = {'message':'Bad command!'}
 
-ACTIONS = {'run-test', 'self-test'}
+ACTIONS = {'run-test', 'self-test', 'run-test-and-analyse'}
 
+
+# TODO: dryrun to test these paths
 TMP = os.path.abspath(r'./tmp')
 TEST_DRIVER = os.path.abspath(r'../../chrome-webpage-profiler/test_driver.py')
+H2_ANALYZER = os.path.abspath(r'../../http2-dump-anatomy/http_traffic_analyzer.py')
+MERGE_TOOL = os.path.abspath(r'../../chrome-webpage-profiler/merge_har.py')
+TSHARK = os.path.abspath(r'../../wireshark-1.99.7/tshark')
+
+ANALYSE_CMD = '{H2_ANALYZER} -g {{pcapfile}} -k {{keyfile}} -b {TSHARK} | {MERGE_TOOL} -h {{harfile}} -o {{finalhar}}'
+ANALYSE_CMD = ANALYSE_CMD.format(H2_ANALYZER=H2_ANALYZER, TSHARK=TSHARK, MERGE_TOOL=MERGE_TOOL)
 #TEST_DRIVER = os.path.abspath(r'/bin/cat')
 
 # NOTE: the key is to avoid unintentional access, not to secure the agent
@@ -28,10 +36,10 @@ def run_test(body):
             os.makedirs(TMP)
         except Exception as _:
             msg = 'Error making output directory: %s', TMP
-            return json.dumps({'message': msg})
+            return json.dumps({'message': msg, 'status': -1})
     if not os.path.isfile(TEST_DRIVER):
         msg = 'No test driver found at %s' % TEST_DRIVER
-        return json.dumps({'message': msg})
+        return json.dumps({'message': msg, 'status': -1})
 
     jobId = "%d"%(time.time()*1000)
     jobIdIndex = jobId[-5:]
@@ -44,14 +52,14 @@ def run_test(body):
             os.makedirs(jobIdIndexPath)
         except Exception as _:
             msg = 'Error making output directory: %s', jobIdIndexPath
-            return json.dumps({'message': msg})
+            return json.dumps({'message': msg, 'status': -2})
 
     if not os.path.isdir(jobIdPath):
         try:
             os.makedirs(jobIdPath)
         except Exception as _:
             msg = 'Error making output directory: %s', jobIdPath
-            return json.dumps({'message': msg})
+            return json.dumps({'message': msg, 'status': -2})
 
     tests = body['tests-config']
     with open(testConfig, 'w') as outfile:
@@ -60,13 +68,80 @@ def run_test(body):
     p = subprocess.Popen([TEST_DRIVER, testConfig], cwd=jobIdPath)
     rc = p.wait()
     if rc == 0:
-        response = {'message': 'OK. Done', 'job-id': jobId}
+        response = {'message': 'OK. Done', 'job-id': jobId,
+                    'status': rc, '_job-path': jobIdPath}
         response['files'] = []
         for f in os.listdir(jobIdPath):
             response['files'].append(os.path.join('/tmp/', jobIdIndex, jobId, f))
         return json.dumps(response)
     else:
-        return json.dumps({'message': 'FAIL. return code%d'%rc})
+        return json.dumps({'message': 'FAIL. return code%d'%rc, 'status': rc})
+
+### the following two helper fuctions are from chrome-webpage-profiler
+### NOTE: remmember to sync them if those are updated
+### SYNC START
+def _sanitize_url(url):
+    '''Returns a version of the URL suitable for use in a file name.'''
+    return re.sub(r'[/\;,><&*:%=+@!#^()|?^]', '-', url)
+
+def _outfile_path(working_dir, url, suffix=None, trial=None):
+    '''Returns a path for an output file (e.g., HAR, screenshot, pcap)'''
+    filename = _sanitize_url(url)
+    if trial:
+        filename += '_trial%d' % trial
+    if suffix:
+        filename += suffix
+    return os.path.join(working_dir, filename)
+### SYNC END
+
+def find_dump_har_pairs(working_dir, config_file):
+    pairs = []
+    with open(config_file, 'r') as f:
+        config = json.load(f)
+
+    for test in config['tests']:
+        pcapFileName = test.get('packet_capture_file_name', test['url'])
+        harFileName = test.get('har_file_name', test['url'])
+        for i in range(0, test.get('num_trials', 1)):
+            pcapFileName = _outfile_path(working_dir, pcapFileName, suffix='.pcap', trial=i)
+            harFileName = _outfile_path(working_dir, harFileName, suffix='.har', trial=i)
+            if (not os.path.isfile(pcapFileName)) or (not os.path.isfile(harFileName)):
+                continue
+            pairs.append([pcapFileName, harFileName])
+
+    return pairs
+
+def do_analyse(dump_file, har_file, key_file):
+    finalHarFile = har_file.split('.har')[0]+'_final.har'
+    cmd = ANALYSE_CMD.format(pcapfile=dump_file, keyfile=key_file, harfile=har_file,finalhar=finalHarFile)
+    # WARNING: security risk: shell=True
+    p = subprocess.Popen(cmd, shell=True)
+    rc = p.wait()
+    if rc == 0:
+        return finalHarFile
+    else:
+        return None
+
+def run_analyse(response):
+    if response['status'] != 0:
+        # bad test
+        return response
+    workingDir = response['_job-path']
+    configFile = os.path.join(workingDir, 'tests.json')
+    if not os.path.isfile(configFile):
+        response['config-file-missing'] = configFile
+        return
+    keyFile = os.path.join(workingDir, 'ssl_keylog')
+    if not os.path.isfile(keyFile):
+        # It is OK not to have a keyfile
+        response['config-file-missing'] = keyFile
+        keyFile = ''
+    pairs = find_dump_har_pairs(workingDir, configFile)
+    finalHars = []
+    for pair in pairs:
+        finalHars.append(do_analyse(pair[0], pair[1], keyFile))
+    response['final-hars'] = finalHars
+    return response
 
 def self_test():
     response = {'message': 'self test done', 'results': {} }
@@ -90,6 +165,9 @@ def execute_POST(body):
 
     if body['action'] == 'run-test':
         return run_test(body)
+    elif body['action'] == 'run-test-and-analyse':
+        response = run_test(body)
+        return run_analyse(response)
     elif body['action'] == 'self-test':
         return self_test()
 
